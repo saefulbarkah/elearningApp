a35ccd0a97fffe1f0b297e4cab4d91504d4d6c32
saefulbarkah <saefulbarkah29062003@gmail.com>
2021-12-12 20:17:29 +0700

    perbaikan lainya

--- resources/views/student/list-task/index.blade.php
+++ resources/views/student/list-task/index.blade.php
@@ -16,22 +16,23 @@
                 </header>
             </div>
             <div class="card-body">
-                <input class="filter form-control" placeholder="cari....." />
+                <input id="myInput" value="" class="filter form-control" placeholder="cari....." />
             </div>
         </div>
     </div>
 </div>
 
 <div class="row">
+    <h3>Tugas Terbaru</h3>
     @foreach ($data as $item)
-    <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="col-lg-6 col-md-6 col-sm-12" id="MyCards">
         <!-- BEGIN PROFILE SIDEBAR -->
             <div class="card">
                 <div class="card-body no-padding height-9">
                     <div class="row">
                     </div>
                     <div class="profile-usertitle">
-                        <div class="profile-usertitle-name">{{ $item->title }}</div>
+                        <div class="profile-usertitle-name title">{{ $item->title }}</div>
                         <div class="profile-usertitle-job"></div>
                     </div>
                     <ul class="list-group list-group-unbordered">
@@ -52,26 +53,30 @@
                     </ul>
                     <br>
                     <a href="" class="btn-sm btn-primary">Kerjakan</a>
-                    <a href="" class="btn-sm btn-success">Lihat tugas</a>
+                    @if (pathinfo($item->file, PATHINFO_EXTENSION) == 'png' && 'jpg')
+                    <a href="{{ asset('images/task/'.$item->file) }}" class="btn-sm btn-primary" target="_blank">Detail</a>
+                    @else
+                    <a href="{{ asset('file/task',$item->file) }}"></a>
+                    @endif
                 </div>
             </div>
         </div>
     @endforeach
-</div>
+<!-- Modal -->
 @push('js')
 <!-- data tables -->
 <script src="{{ asset('assets/bundles/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('assets/bundles/datatables/plugins/bootstrap/dataTables.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('assets/data/table-data.js') }}"></script>
 <script>
-    $(document).ready(function(){
-      $("#myInput").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-      });
+$(document).ready(function() {
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#MyCards .card").filter(function() {
+      $(this).toggle($(this).find('.title').text().toLowerCase().indexOf(value) > -1)
     });
+  });
+});
 </script>
 @endpush
 @endsection
